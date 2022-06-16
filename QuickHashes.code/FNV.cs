3997911044c53ae4fa9eb73088b054f50f60acf3
Francisco Moreno <ingfrancisco.moreno1@gmail.com>
2022-06-16 23:54:01 +0200

    Feat: FNV hash (#4)
    * (feat) add FNV,  FNV Alternate and Interface
    
    * (feat) add tests for FNV Hashes classes
    
    * Style: FNV hash
    
    Co-authored-by: restyled-io[bot] <32688539+restyled-io[bot]@users.noreply.github.com>

--- QuickHashes.code/FNV.cs
+++ QuickHashes.code/FNV.cs
@@ -1,7 +1,18 @@
-﻿using System;
+﻿using System.Linq;
+using System;
 
 namespace QuickHashes.code {
-  public class FNV : IHash {
-    public ulong GetHash() { throw new NotImplementedException(); }
+  public class FNV : IFNV {
+    public ulong GetHash(ulong dataToHash) =>
+        BitConverter.GetBytes(dataToHash)
+            .Aggregate(14695981039346656037,
+                       (accum, current) => (accum * 1099511628211) ^ current);
+
+    public uint GetHash(uint dataToHash) =>
+        BitConverter.GetBytes(dataToHash)
+            .Aggregate(216613626U,
+                       (accum, current) => (accum * 16777619U) ^ current);
+
+    public long GetHash(long dataToHash) => this.GetHash((uint)dataToHash);
   }
 }

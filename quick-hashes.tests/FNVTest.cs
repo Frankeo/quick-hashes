11d297df596b181c021e8aec90e59f66b96428f9
Francisco Moreno <ingfrancisco.moreno1@gmail.com>
2022-06-16 17:44:39 +0200

    build: setup Init  (#1)
    * build: Init setup with tests and code base project
    
    * style: Init setup  (#2)

---
+++ quick-hashes.tests/FNVTest.cs
@@ -0,0 +1,12 @@
+using System;
+using Xunit;
+
+namespace quick_hashes.tests {
+  public class FNVTest {
+    [Fact]
+    public void ComputeSameHashSameWords() {}
+
+    [Fact]
+    public void Compute2DifferentHashesDifferentWords() {}
+  }
+}

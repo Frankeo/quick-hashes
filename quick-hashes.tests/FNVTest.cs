4490feb96d56ec5c29a328336074ee87ea805133
Frankeo <ingfrancisco.moreno1@gmail.com>
2022-06-16 15:30:12 +0000

    Init setup with tests and code base project

---
+++ quick-hashes.tests/FNVTest.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace quick_hashes.tests
+{
+    public class FNVTest
+    {
+        [Fact]
+        public void ComputeSameHashSameWords()
+        {
+
+        }
+
+        [Fact]
+        public void Compute2DifferentHashesDifferentWords()
+        {
+
+        }
+    }
+}
